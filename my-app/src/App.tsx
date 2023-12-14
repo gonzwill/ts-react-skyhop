a36babbc372e03594761bdfe8036a4aa416c05c6
gonzwill <willgonzalez2@gmail.com>
2023-12-14 12:26:16 -0500

    Initialized ts/react project and added TailwindCSS

---
+++ my-app/src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <header className="">
+        <p className='bg-red-100 text-blue-200'>
+          Edit <code>src/App.tsx</code> and save to reload. Hi
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
