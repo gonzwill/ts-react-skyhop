91d6da07ca281fea3b58a9293f4ab58c68747f1d
gonzwill <willgonzalez2@gmail.com>
2023-12-14 14:40:48 -0500

    Created Dropdown form element, WIP still have to add support for dropdown element selection

---
+++ my-app/src/components/formComponents/DropdownElement.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from 'react';
+
+interface DropdownElementProps {
+  currentChoice: string;
+  defaultText: string;
+}
+
+const DropdownElement: React.FC<DropdownElementProps> = ({ currentChoice, defaultText }) => {
+  const [showDropdownOptions, setShowDropdownOptions] = useState<boolean>(false);
+
+  return (
+    <div className='w-full py-1 select-none'>
+      <div 
+        className='flex flex-row justify-between items-center rounded-md border border-[#BDC1C7] cursor-pointer'
+      >
+        <div className='pl-3 text-[#092D4E] text-[11px] font-extralight'>
+          {currentChoice !== null && currentChoice !== '' ? currentChoice : defaultText }
+        </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className='h-6 w-6 pr-3 text-[#303144]'
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
+      </div>
+    </div>
+  );
+}
+
+export default DropdownElement;
