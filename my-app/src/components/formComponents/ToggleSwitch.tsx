57485b8e74af7db526b6fa49eb9e0f088752e828
gonzwill <willgonzalez2@gmail.com>
2023-12-14 16:59:58 -0500

    Implemented ToggleSwitch form element

---
+++ my-app/src/components/formComponents/ToggleSwitch.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+interface ToggleSwitchProps {
+  checkedValue: boolean;
+  handleToggle: () => void;
+}
+
+const ToggleSwitch: React.FC<ToggleSwitchProps> = ({ checkedValue, handleToggle }) => {
+  return (
+    <div className="relative w-[38px] flex justify-between items-center cursor-pointer" onClick={handleToggle}>
+      <input 
+        checked={checkedValue === true}
+        className="absolute left-1/2 -translate-x-1/2 w-[full] h-full peer appearance-none rounded-md" 
+        type="checkbox" 
+      />
+      <span className="w-9 h-5 flex items-center pl-[1px] bg-gray-300 rounded-full duration-200 ease-in-out peer-checked:bg-[#193E6F] after:w-[18px] after:h-[18px] after:bg-white after:rounded-full after:duration-200 peer-checked:after:translate-x-[16px]"></span>
+    </div>
+  );
+}
+
+export default ToggleSwitch;
