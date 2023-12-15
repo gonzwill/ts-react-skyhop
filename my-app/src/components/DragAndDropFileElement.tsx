621de0ae3a9402e2a84b25143bc98ce6f9c46dea
gonzwill <willgonzalez2@gmail.com>
2023-12-15 07:26:36 -0500

    Added support for Browse files, WIP for handleBrowseFileChange to save file names on DocumentUploadModal

--- my-app/src/components/DragAndDropFileElement.tsx
+++ my-app/src/components/DragAndDropFileElement.tsx
@@ -1,8 +1,15 @@
-import React, { DragEvent, useState } from 'react';
+import React, { ChangeEvent, DragEvent, useRef, useState } from 'react';
 
 const DragAndDropFileElement: React.FC = () => {
   const [isDraggingFile, setIsDraggingFile] = useState<boolean>(false);
+  const browseFileInputRef = useRef<HTMLInputElement | null>(null);
 
+  const handleBrowseFilesClick = () => {
+    if (browseFileInputRef.current) {
+      browseFileInputRef.current.click();
+    }
+  };
+  
   const handleDragEnter = (e: DragEvent<HTMLDivElement>) => {
     e.preventDefault();
     setIsDraggingFile(true);
@@ -17,21 +24,33 @@ const DragAndDropFileElement: React.FC = () => {
     setIsDraggingFile(false);
   };
 
+  const handleBrowseFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files) {
+      const files: File[] = Array.from(e.target.files);
+    }
+  };
+
   return (
     <div className='h-[130px] w-full flex flex-col items-center border border-[#BCBDBE] rounded-md'>
       <div 
-        className={`h-4/6 w-11/12 flex justify-center items-center mt-3 rounded-md ${isDraggingFile && 'bg-gray-50'}`}
+        className={`h-4/6 w-11/12 flex justify-center items-center mt-3 rounded-md ${isDraggingFile && 'bg-gray-100'}`}
         onDragEnter={handleDragEnter}
         onDragOver={handleDragOver}
         onDragLeave={handleDragLeave}
       >
+        <input 
+          className='hidden'
+          onChange={handleBrowseFileChange}
+          ref={browseFileInputRef}
+          type='file'
+        />
         <div
           className='flex flex-col justify-center items-center rounded-md border border-dashed border-[#AFB0B1]'
           style={{height: isDraggingFile ? '90%': '95%' , width: isDraggingFile ? '95%': '98%' }}
         >
           <img className='h-3 w-3' src='https://i.imgur.com/NTowAnd.png' />
           <div className='mt-1 text-[#082C4D] text-[10px] font-extralight'>
-            Drag & Drop Here or&nbsp;<span className='font-bold cursor-pointer hover:underline'>Browse</span>
+            Drag & Drop Here or&nbsp;<span className='font-bold cursor-pointer hover:underline' onClick={handleBrowseFilesClick}>Browse</span>
           </div>
         </div>
       </div>
